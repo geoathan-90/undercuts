b84a2485c637f372c812eb5892a913a4e95426d0
geoathan-90 <32165455+geoathan-90@users.noreply.github.com>
2026-05-12 20:37:10 +0300

    progressed

--- loads.py
+++ loads.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def ice_load(diam, ice, ice_density=900):
     diam = diam / 1000          # Convert diameter from mm to m
@@ -21,7 +22,7 @@ def total_load(diam, ice, ice_density, wind, gravity, w):
 
 ##################
 
-def Vor(wh, H, B_mon, phi, T, alpha, wv, diam, wind, ice, gravity, w):
+def Vor(H, B_mon, phi, T, alpha, diam, wind, ice, ice_density, gravity, w):
 
     wh = wind_load(diam, wind, ice, gravity, w)
     wv = w + ice_load(diam, ice, ice_density=900)
@@ -30,19 +31,55 @@ def Vor(wh, H, B_mon, phi, T, alpha, wv, diam, wind, ice, gravity, w):
 
 def main():
     
-    S = 290            # m
-    h = 0              # m
-    Th = 2875          # kg
-    w = 1.303          # kg/m
-    diam = 25.15       # mm
-    ice = .25          # inches
-    ice_density = 900  # kg/m^3
-    wind = 44          # kg/m2
-    gravity = 9.810665 # m/s^2
+    S = 290                 # m
+    h = 0                   # m
+    Th = 2875               # kg
+    
+    w = 1.303               # kg/m
+    diam = 25.15            # mm
+    ice = 0                 # inches
+    ice_density = 900       # kg/m^3
+    wind = 32               # kg/m2
+    gravity = 9.810665      # m/s^2
 
-    print(ice_load(diam, ice, ice_density))
-    print(wind_load(diam, wind, ice, gravity, w))
-    print(total_load(diam, ice, ice_density, wind, gravity, w))
+    phi = np.radians(35)    # the input in degrees 
+    T = 1720               # ruling span tension
+    B_mon = 100              # kg     
+    alpha = np.radians(0)   # the input in degrees
+    
+    H = np.linspace(100,450,351)
+    alpha_degrees = [0, 1, 2, 3, 4, 5, 6]
+
+    plt.figure(figsize=(8, 5))
+
+    for alpha_deg in alpha_degrees:
+        alpha = np.radians(alpha_deg)
+
+        V = Vor(H,B_mon, phi, T, alpha, diam, wind, ice, ice_density, gravity, w)
+
+        plt.plot(H, V, label=f"γωνία γραμμής = {alpha_deg}°")
+
+    plt.xlabel("Οριζόντιο")
+    plt.ylabel("Κατακόρυφο")
+    plt.title("")
+    #plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.ylim(bottom=100)
+    plt.show()
+
+    
+    #V = Vor(H,B_mon, phi, T, alpha, diam, wind, ice, ice_density, gravity, w)
+    
+    #plt.plot(H,V)
+    
+    #print(Vor(145,B_mon, phi, T, alpha, diam, wind, ice, ice_density, gravity, w))
+    #rint(Vor(450,B_mon, phi, T, alpha, diam, wind, ice, ice_density, gravity, w))
+    
+    
+    #print(ice_load(diam, ice, ice_density))
+    #print(wind_load(diam, wind, ice, gravity, w))
+    #print(total_load(diam, ice, ice_density, wind, gravity, w))
 
     #plt.plot([1, 2, 3], [1, 4, 11])
     #plt.savefig("plot.png")
